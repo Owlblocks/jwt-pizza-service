c10db7e7eebe2e3e965d086ee29832f133950afc
= <d-weatherford@live.com>
2024-10-02 00:12:24 -0600

    fix(eslint.config.mjs)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import pluginJs from "@eslint/js";
 
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  { languageOptions: { globals: globals.node } },
   {languageOptions: { globals: globals.jest }},
   pluginJs.configs.recommended,
 ];
